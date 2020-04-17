8fde39c873728ac3c1aa28192138d8a26cdff1f6
张奇峰 <10515935zwj>
2020-04-17 10:04:21 +0800

    完善linux环境信号监听逻辑

--- App/Core/Destruct/Destroy.go
+++ App/Core/Destruct/Destroy.go
@@ -12,10 +12,8 @@ func init() {
 	//  用于系统信号的监听
 	go func() {
 		c := make(chan os.Signal)
-		// 监听信号
-		signal.Notify(c)
-		received := <-c
-
+		signal.Notify(c) // 监听信号
+		received := <-c  //接收信号管道中的值
 		switch received {
 		case os.Interrupt, os.Kill, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGILL, syscall.SIGTERM:
 			// 检测到程序退出时，按照键的前缀统一执行销毁类事件
