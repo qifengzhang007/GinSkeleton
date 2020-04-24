de6b65b8c873a40c3986b3ec348d6acc00eb80de
张奇峰 <10515935zwj>
2020-04-24 15:51:53 +0800

    删除调试代码

--- App/Service/UploadFile/UploadFile.go
+++ App/Service/UploadFile/UploadFile.go
@@ -18,11 +18,11 @@ func Upload(context *gin.Context, save_path string) bool {
 
 	//  1.获取上传的文件名(参数验证器已经验证完成了第一步错误，这里简化)
 	file, _ := context.FormFile(Variable.UploadFileField) //  file 是一个文件结构体（文件对象）
-	var save_file_name string
+
 	//  保存文件，原始文件名进行全局唯一编码加密、md5 加密，保证在后台存储不重复
 	var save_err error
 	if unique_id, err := SnowFlake.CreateSnowFlakeFactory().GetId(); err == nil {
-		save_file_name := fmt.Sprintf("%d%s", unique_id, save_file_name)
+		save_file_name := fmt.Sprintf("%d%s", unique_id, file.Filename)
 		save_file_name = MD5Cryt.MD5([]byte(save_file_name)) + path.Ext(save_file_name)
 		if save_err = context.SaveUploadedFile(file, save_path+save_file_name); save_err == nil {
 			//  上传成功,返回资源的存储路径，这里请根据实际返回绝对路径或者相对路径
