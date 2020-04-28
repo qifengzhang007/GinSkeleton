098fa1a17521387898d267e7324187c156295b7a
张奇峰 <zhangqf@inesadt.com>
2020-04-29 01:17:25 +0800

    格式化代码

--- App/Service/Websocket/Ws.go
+++ App/Service/Websocket/Ws.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"log"
 )
 
 type Ws struct {
@@ -40,23 +39,23 @@ func (w *Ws) OnError(err error) {
 
 	//err 接收消息期间发生的错误（一般来说，远端掉线、断开、卡死等会触发此错误）
 	fmt.Printf("ws连接消息轮训期间发生错误: %v", err.Error())
-	if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-		log.Printf("error: %v", err)
-	}
 }
 
 // OnClose 客户端关闭回调（主要有：下线、断开、卡死等无法正常通讯的状况）
 func (w *Ws) OnClose() {
+
 	w.WsClient.Hub.UnRegister <- w.WsClient // 向hub管道投递一条注销消息，有hub中心负责关闭连接、删除在线数据
 }
 
 //获取在线的全部客户端
 func (w *Ws) GetOnlineClients() {
+
 	fmt.Printf("在线客户端数量：%d\n", len(w.WsClient.Hub.Clients))
 }
 
 // 向全部在线客户端广播消息
 func (w *Ws) BroadcastMsg(send_msg string) {
+
 	//获取每一个在线的客户端，向远端发送消息
 	for online_client, _ := range w.WsClient.Hub.Clients {
 		online_client.Conn.WriteMessage(websocket.TextMessage, []byte(send_msg))
