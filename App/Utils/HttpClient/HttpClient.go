ba03be15ed93af920a51243450103e08d1eaac05
张奇峰 <zhangqf@inesadt.com>
2020-05-11 02:01:26 +0800

    提交单元测试代码调试版到dev分支

--- App/Utils/HttpClient/HttpClient.go
+++ App/Utils/HttpClient/HttpClient.go
@@ -4,6 +4,24 @@ import (
 	"github.com/qifengzhang007/goz"
 )
 
-func CreateClient() *goz.Request {
-	return goz.NewClient()
+func CreateClient(options ...goz.Options) *goz.Request {
+	if len(options) == 0 {
+		return goz.NewClient(DefaultHeader())
+	} else {
+		return goz.NewClient(options...)
+	}
+
+}
+func DefaultHeader(options ...goz.Options) goz.Options {
+	headers := goz.Options{
+		Headers: map[string]interface{}{
+			"User-Agent":                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.5977.400 LBBROWSER/10.1.3752.400",
+			"Accept":                    "text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+			"Accept-Encoding":           "gzip, deflate",
+			"Accept-Language":           "zh-CN,zh;q=0.9",
+			"Upgrade-Insecure-Requests": "1",
+			"Cache-Control":             "max-age=0",
+		},
+	}
+	return headers
 }
--- Test/Unit_test.go
+++ Test/Unit_test.go
@@ -11,20 +11,22 @@ import (
 func ExampleHttpClientTest() {
 	cli := HttpClient.CreateClient()
 	//http
-	if resp, err := cli.Get("http://101.132.69.236/api/v2/test_network"); err == nil {
-		centent, _ := resp.GetContents()
-		fmt.Printf("%v", centent)
-		// Output:  {"code":200,"msg":"OK","data":""}153.99.181.48{"code":200,"msg":"OK","data":""}
-	}
-	//https
-	if resp, err := cli.Get("https://www.fbisb.com/ip.php"); err == nil {
-		centent, _ := resp.GetContents()
-		fmt.Printf("%v", centent)
-		// Output:  153.99.181.48
-	}
+	//resp, _ := cli.Get("http://101.132.69.236/api/v2/test_network");
+	//centent, _ := resp.GetContents()
+	//fmt.Printf("%v", centent)
+	// Output: {"code":200,"msg":"OK","data":""}
+
+	//	// Output:  {"code":200,"msg":"OK","data":""}153.99.181.48{"code":200,"msg":"OK","data":""}
+	//}
+	////https
+	//if resp, err := cli.Get("https://www.fbisb.com/ip.php"); err == nil {
+	//	centent, _ := resp.GetContents()
+	//	fmt.Printf("%v", centent)
+	//	// Output:  153.99.181.48
+	//}
 	// post
-	resp, _ := cli.Post("http://101.132.69.236/api/v2/test_network")
-	centent, _ := resp.GetContents()
-	fmt.Printf("%v", centent)
-	// Output:  {"code":200,"msg":"OK","data":""}153.99.181.48{"code":200,"msg":"OK","data":""}
+	resp2, _ := cli.Post("http://101.132.69.236/api/v2/test_network")
+	centent2, _ := resp2.GetContents()
+	fmt.Printf("%s", centent2)
+	//Output: {"code":200,"msg":"OK","data":""}
 }
