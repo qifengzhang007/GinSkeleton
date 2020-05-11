6fe9ac8313be3eb4ae402b6e182a265c338a7477
张奇峰 <zhangqf@inesadt.com>
2020-05-12 01:43:23 +0800

    增加HttpClient，完成度：98%...，网页请求结果gbk转utf暂时处于调试阶段... 提交至dev分支..

--- App/Utils/HttpClient/HttpClient.go
+++ App/Utils/HttpClient/HttpClient.go
@@ -4,24 +4,8 @@ import (
 	"github.com/qifengzhang007/goz"
 )
 
-func CreateClient(options ...goz.Options) *goz.Request {
-	if len(options) == 0 {
-		return goz.NewClient(DefaultHeader())
-	} else {
-		return goz.NewClient(options...)
-	}
+//  httpClient 文档地址： https://github.com/qifengzhang007/goz
 
-}
-func DefaultHeader(options ...goz.Options) goz.Options {
-	headers := goz.Options{
-		Headers: map[string]interface{}{
-			"User-Agent":                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.5977.400 LBBROWSER/10.1.3752.400",
-			"Accept":                    "text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-			"Accept-Encoding":           "gzip, deflate",
-			"Accept-Language":           "zh-CN,zh;q=0.9",
-			"Upgrade-Insecure-Requests": "1",
-			"Cache-Control":             "max-age=0",
-		},
-	}
-	return headers
+func CreateClient(options ...goz.Options) *goz.Request {
+	return goz.NewClient(options...)
 }
--- Test/Unit_test.go
+++ Test/Unit_test.go
@@ -26,7 +26,22 @@ func ExampleHttpClientTest() {
 	//}
 	// post
 	resp2, _ := cli.Post("http://101.132.69.236/api/v2/test_network")
+	//fmt.Printf("%#v\n",resp2.GetRequest().Header)
 	centent2, _ := resp2.GetContents()
 	fmt.Printf("%s", centent2)
 	//Output: {"code":200,"msg":"OK","data":""}
+}
+
+// 测试 httpclient header头字段合并
+func ExampleHeaders() {
+	//v2:=goz.Options{
+	//	Headers: map[string]interface{}{
+	//		"Content-type":"application/json",
+	//	},
+	//}
+	cli := HttpClient.CreateClient()
+	resp, _ := cli.Get("http://news.baidu.com/passport")
+	txt := resp.GetReasonPhrase()
+	fmt.Print(txt)
+	//Output: OK
 }
