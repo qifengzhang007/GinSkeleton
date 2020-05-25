9592d988472519a043010d314a72563716b1f0ca
张奇峰 <zhangqf@inesadt.com>
2020-05-25 23:58:56 +0800

    增加cobra子命令文件

---
+++ Cli/cmd/subCmd.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// 定义子命令
+var subCmd = &cobra.Command{
+	Use:   "subCmd",
+	Short: "subCmd 命令简要介绍",
+	Long:  `命令使用详细介绍`,
+	Args:  cobra.ExactArgs(1), //  限制非flag参数的个数 = 1 ,超过1个会报错
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("测试子命令被嵌套调用：" + args[0])
+	},
+}
+
+//注册子命令
+func init() {
+	demo.AddCommand(subCmd)
+	// 子命令仍然可以定义 flag 参数，相关语法参见 demo.go 文件
+}
