30a05ebf493fe94363efe4c01ee8c1247143d46a
张奇峰 <10515935zwj>
2020-06-03 14:49:56 +0800

    HttpClient相关备注更新至新库地址

--- App/Utils/HttpClient/HttpClient.go
+++ App/Utils/HttpClient/HttpClient.go
@@ -4,7 +4,7 @@ import (
 	"github.com/qifengzhang007/goCurl"
 )
 
-//  httpClient 文档地址： https://github.com/qifengzhang007/goz
+//  httpClient 文档地址： https://gitee.com/daitougege/goCurl
 
 func CreateClient(options ...goCurl.Options) *goCurl.Request {
 	return goCurl.NewClient(options...)
