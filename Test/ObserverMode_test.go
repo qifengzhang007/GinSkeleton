8d01580f772e063c616d97c4b35ed1c0ba57cac0
张奇峰 <10515935zwj>
2020-06-04 18:20:34 +0800

    增加测试注释

--- Test/ObserverMode_test.go
+++ Test/ObserverMode_test.go
@@ -19,6 +19,7 @@ func (c *Observer2) Update(subject *ObserverMode.Subject) {
 	fmt.Printf("observer2参数：%v\n", subject.GetParams())
 }
 
+// 观察者模式目前开始测试，未通过，dev 分支的代码请勿使用
 func ExampleObseerverTest() {
 
 	subjectHub := &ObserverMode.Subject{}
