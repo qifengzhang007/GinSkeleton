c01d1379620f5ac9495a9fe5e5990ac1ac867634
张奇峰 <10515935zwj>
2020-08-20 17:38:35 +0800

    修复一处路径大写问题->小写。

--- app/global/variable/variable.go
+++ app/global/variable/variable.go
@@ -13,7 +13,7 @@ var (
 	EventDestroyPrefix = "Destroy_" //  程序退出时需要销毁的事件前缀
 	//上传文件保存路径
 	UploadFileField    = "files"                  // post上传文件时，表单的键名
-	UploadFileSavePath = "/Storage/app/uploaded/" // 该路径与 BasePath 进行拼接使用
+	UploadFileSavePath = "/storage/app/uploaded/" // 该路径与 BasePath 进行拼接使用
 
 	//日志存储路径
 	ZapLog *zap.Logger //  全局日志句柄
