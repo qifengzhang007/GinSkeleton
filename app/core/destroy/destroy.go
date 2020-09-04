14dfdea91c9874c0e6bea084d40ac2c2b549877a
张奇峰 <10515935zwj>
2020-09-04 09:35:59 +0800

    进程收到被结束信号退出时完善

--- app/core/destroy/destroy.go
+++ app/core/destroy/destroy.go
@@ -2,6 +2,7 @@ package destroy
 
 import (
 	"go.uber.org/zap"
+	"goskeleton/app/core/event_manage"
 	"goskeleton/app/global/consts"
 	"goskeleton/app/global/variable"
 	"os"
@@ -16,6 +17,7 @@ func init() {
 		signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM) // 监听可能的退出信号
 		received := <-c                                                                           //接收信号管道中的值
 		variable.ZapLog.Warn(consts.ProcessKilled, zap.String("信号值", received.String()))
+		(event_manage.CreateEventManageFactory()).FuzzyCall(variable.EventDestroyPrefix)
 		close(c)
 		os.Exit(1)
 	}()
