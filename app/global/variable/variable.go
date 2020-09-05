cc82d32bcb54dae71cad35b67cc196c29d13e769
张奇峰 <zhangqf@inesadt.com>
2020-09-05 22:22:43 +0800

    增强判断程序所处的运行环境模式：正常运行还是单元测试,从而更好的初始化相关全局变量,例如 BasePath

--- app/global/variable/variable.go
+++ app/global/variable/variable.go
@@ -30,7 +30,7 @@ func init() {
 	// 1.初始化程序根目录
 	if path, err := os.Getwd(); err == nil {
 		// 路径进行处理，兼容单元测试程序程序启动时的奇怪路径
-		if len(os.Args) > 1 && os.Args[1] == "-test.v" {
+		if len(os.Args) > 1 && strings.HasPrefix(os.Args[1], "-test") {
 			BasePath = strings.Replace(strings.Replace(path, `\test`, "", 1), `/test`, "", 1)
 		} else {
 			BasePath = path
