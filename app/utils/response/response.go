b4623af6e10c3195ef3d80bce7bb85481e968a3c
张奇峰 <10515935zwj>
2020-10-14 13:46:30 +0800

    response.go 增加快捷函数,对细节进行稍作完善

--- app/utils/response/response.go
+++ app/utils/response/response.go
@@ -49,7 +49,7 @@ func SuccessReturnMsg(c *gin.Context, msg string) {
 // -------------------------- 简易接口错误返回 ----------------------------------
 // 业务内逻辑异常
 func ErrorReturnMsg(c *gin.Context, code int, msg string) {
-	ReturnJson(c, http.StatusOK, code, msg, gin.H{})
+	ReturnJson(c, http.StatusBadRequest, code, msg, "")
 }
 
 func ErrorAuthFail(c *gin.Context) {
@@ -59,5 +59,5 @@ func ErrorAuthFail(c *gin.Context) {
 }
 
 func ErrorParam(c *gin.Context, wrongParam interface{}) {
-	ReturnJson(c, http.StatusOK, consts.CurdStatusOkCode, consts.CurdStatusOkMsg, wrongParam)
+	ReturnJson(c, http.StatusBadRequest, consts.ValidatorParamsCheckFailCode, consts.ValidatorParamsCheckFailMsg, wrongParam)
 }
