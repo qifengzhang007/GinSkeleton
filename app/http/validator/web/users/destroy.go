98767cb2aece1054ba52519c14f5c0d47cd5d874
张奇峰 <1990966837@qq.com>
2020-12-07 12:43:10 +0800

    表单参数验证器注释更新，便于新手阅读、理解

--- app/http/validator/web/users/destroy.go
+++ app/http/validator/web/users/destroy.go
@@ -25,7 +25,7 @@ func (d Destroy) CheckParams(context *gin.Context) {
 		return
 	}
 
-	//  该函数主要是将本结构体的字段（成员）按照 consts.ValidatorPrefix+ json标签对应的 键 => 值 形式直接传递给下一步（控制器）
+	//  该函数主要是将本结构体的字段（成员）按照 consts.ValidatorPrefix+ json标签对应的 键 => 值 形式绑定在上下文，便于下一步（控制器）可以直接通过 context.Get(键) 获取相关值
 	extraAddBindDataContext := data_transfer.DataAddContext(d, consts.ValidatorPrefix, context)
 	if extraAddBindDataContext == nil {
 		response.ErrorSystem(context, "UserShow表单参数验证器json化失败", "")
--- app/http/validator/web/users/login.go
+++ app/http/validator/web/users/login.go
@@ -28,7 +28,7 @@ func (l Login) CheckParams(context *gin.Context) {
 		return
 	}
 
-	//  该函数主要是将本结构体的字段（成员）按照 consts.ValidatorPrefix+ json标签对应的 键 => 值 形式直接传递给下一步（控制器）
+	//  该函数主要是将本结构体的字段（成员）按照 consts.ValidatorPrefix+ json标签对应的 键 => 值 形式绑定在上下文，便于下一步（控制器）可以直接通过 context.Get(键) 获取相关值
 	extraAddBindDataContext := data_transfer.DataAddContext(l, consts.ValidatorPrefix, context)
 	if extraAddBindDataContext == nil {
 		response.ErrorSystem(context, "userLogin表单验证器json化失败", "")
