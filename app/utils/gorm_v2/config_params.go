ff6256ed3764999aaf00f34fe49cc9dd218cf718
张奇峰 <1990966837@qq.com>
2021-01-18 14:11:26 +0800

    注释更新

--- app/utils/gorm_v2/config_params.go
+++ app/utils/gorm_v2/config_params.go
@@ -2,7 +2,7 @@ package gorm_v2
 
 // 数据库参数配置，结构体
 // 用于解决复杂的业务场景连接到多台服务器部署的 mysql、sqlserver、postgresql 数据库
-// 具体用法参见单元测试部分：TestCustomeParamsConnMysql 函数代码段
+// 具体用法参见单元测试(test/gormv2_test.go)文件，TestCustomeParamsConnMysql 函数代码段
 
 type ConfigParams struct {
 	Write ConfigParamsDetail
