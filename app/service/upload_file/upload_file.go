f556c3ea82c335f921d8d14a762c98171c0d0913
飘渺 <17521302119>
2021-03-23 01:00:16 +0800

    1.为了更好地处理文件上传细节，程序增加自动创建软连接功能，同时文件上传相关代码更新以及配置项部分更新.

--- app/service/upload_file/upload_file.go
+++ app/service/upload_file/upload_file.go
@@ -4,16 +4,18 @@ import (
 	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"goskeleton/app/global/consts"
 	"goskeleton/app/global/my_errors"
 	"goskeleton/app/global/variable"
 	"goskeleton/app/utils/md5_encrypt"
-	"goskeleton/app/utils/response"
+	"os"
 	"path"
 	"strings"
+	"time"
 )
 
-func Upload(context *gin.Context, savePath string) bool {
+func Upload(context *gin.Context, savePath string) (r bool, finnalSavePath interface{}) {
+
+	newSavePath, newReturnPath := generateYearMonthPath(savePath)
 
 	//  1.获取上传的文件名(参数验证器已经验证完成了第一步错误，这里简化)
 	file, _ := context.FormFile(variable.ConfigYml.GetString("FileUploadSetting.UploadFileField")) //  file 是一个文件结构体（文件对象）
@@ -24,18 +26,33 @@ func Upload(context *gin.Context, savePath string) bool {
 		saveFileName := fmt.Sprintf("%d%s", sequence, file.Filename)
 		saveFileName = md5_encrypt.MD5(saveFileName) + path.Ext(saveFileName)
 
-		if saveErr = context.SaveUploadedFile(file, savePath+saveFileName); saveErr == nil {
+		if saveErr = context.SaveUploadedFile(file, newSavePath+saveFileName); saveErr == nil {
 			//  上传成功,返回资源的相对路径，这里请根据实际返回绝对路径或者相对路径
-			success := gin.H{
-				"path": strings.ReplaceAll(savePath+saveFileName, variable.BasePath, ""),
+			finnalSavePath = gin.H{
+				"path": "/public" + strings.ReplaceAll(newReturnPath+saveFileName, variable.BasePath, ""),
 			}
-			response.Success(context, consts.CurdStatusOkMsg, success)
-			return true
+			return true, finnalSavePath
 		}
 	} else {
 		saveErr = errors.New(my_errors.ErrorsSnowflakeGetIdFail)
+		variable.ZapLog.Error("文件保存出错：" + saveErr.Error())
 	}
-	response.Fail(context, consts.FilesUploadFailCode, consts.FilesUploadFailMsg+", 文件保存失败!", saveErr.Error())
-	return false
+	return false, nil
 
+}
+
+// 文件上传可以设置按照 xxx年-xx月 格式存储
+func generateYearMonthPath(savePathPre string) (string, string) {
+	returnPath := variable.BasePath + variable.ConfigYml.GetString("FileUploadSetting.UploadFileReturnPath")
+	curYearMonth := time.Now().Format("2006_01")
+	newSavePathPre := savePathPre + curYearMonth
+	newReturnPathPre := returnPath + curYearMonth
+	// 相关路径不存在，创建目录
+	if _, err := os.Stat(newSavePathPre); err != nil {
+		if err = os.MkdirAll(newSavePathPre, 0666); err != nil {
+			variable.ZapLog.Error("文件上传创建目录出错" + err.Error())
+			return "", ""
+		}
+	}
+	return newSavePathPre + "/", newReturnPathPre + "/"
 }
