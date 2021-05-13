0569b5113d03e3b02f80d94d86471a71d01b217c
张奇峰 <1990966837@qq.com>
2021-05-13 10:02:29 +0800

    变量名称调整,避免与系统包名类似

--- app/global/variable/variable.go
+++ app/global/variable/variable.go
@@ -49,12 +49,12 @@ var (
 
 func init() {
 	// 1.初始化程序根目录
-	if path, err := os.Getwd(); err == nil {
+	if curPath, err := os.Getwd(); err == nil {
 		// 路径进行处理，兼容单元测试程序程序启动时的奇怪路径
 		if len(os.Args) > 1 && strings.HasPrefix(os.Args[1], "-test") {
-			BasePath = strings.Replace(strings.Replace(path, `\test`, "", 1), `/test`, "", 1)
+			BasePath = strings.Replace(strings.Replace(curPath, `\test`, "", 1), `/test`, "", 1)
 		} else {
-			BasePath = path
+			BasePath = curPath
 		}
 	} else {
 		log.Fatal(my_errors.ErrorsBasePath)
