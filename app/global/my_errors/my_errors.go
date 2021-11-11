e80e0ef1e32c4a79034e4e7e3f4314177b2ddd81
张奇峰 <1990966837@qq.com>
2021-11-11 13:01:10 +0800

    新增一项错误常量

--- app/global/my_errors/my_errors.go
+++ app/global/my_errors/my_errors.go
@@ -27,6 +27,7 @@ const (
 	ErrorsDbDriverNotExists        string = "数据库驱动类型不存在,目前支持的数据库类型：mysql、sqlserver、postgresql，您提交数据库类型："
 	ErrorsDialectorDbInitFail      string = "gorm dialector 初始化失败,dbType:"
 	ErrorsGormDBCreateParamsNotPtr string = "gorm Create 函数的参数必须是一个指针"
+	ErrorsGormDBUpdateParamsNotPtr string = "gorm 的 Update、Save 函数的参数必须是一个指针(GinSkeleton ≥ v1.5.29 版本新增验证，为了完美支持 gorm 的所有回调函数,请在参数前面添加 & )"
 	//redis部分
 	ErrorsRedisInitConnFail string = "初始化redis连接池失败"
 	ErrorsRedisAuthFail     string = "Redis Auth 鉴权失败，密码错误"
