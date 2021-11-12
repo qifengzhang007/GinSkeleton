c419a55d426aae847e9d9b304cd4e86f0adaa0b6
张奇峰 <1990966837@qq.com>
2021-11-12 10:24:02 +0800

    更新字段格式为数据库小写格式

--- app/utils/gorm_v2/hook.go
+++ app/utils/gorm_v2/hook.go
@@ -23,8 +23,8 @@ func CreateBeforeHook(gormDB *gorm.DB) {
 		_ = gormDB.AddError(errors.New(my_errors.ErrorsGormDBCreateParamsNotPtr))
 	} else {
 		// 参数校验无错误自动设置 CreatedAt、 UpdatedAt
-		gormDB.Statement.SetColumn("CreatedAt", time.Now().Format(variable.DateFormat))
-		gormDB.Statement.SetColumn("UpdatedAt", time.Now().Format(variable.DateFormat))
+		gormDB.Statement.SetColumn("created_at", time.Now().Format(variable.DateFormat))
+		gormDB.Statement.SetColumn("updated_at", time.Now().Format(variable.DateFormat))
 	}
 }
 
@@ -38,6 +38,6 @@ func UpdateBeforeHook(gormDB *gorm.DB) {
 		_ = gormDB.AddError(errors.New(my_errors.ErrorsGormDBUpdateParamsNotPtr))
 	} else {
 		// 参数校验无错误自动设置 UpdatedAt
-		gormDB.Statement.SetColumn("UpdatedAt", time.Now().Format(variable.DateFormat))
+		gormDB.Statement.SetColumn("updated_at", time.Now().Format(variable.DateFormat))
 	}
 }
