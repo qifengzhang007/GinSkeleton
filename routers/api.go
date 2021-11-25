e0df863e880208954fcaa473df9598597a53cff8
张奇峰 <1990966837@qq.com>
2021-11-26 00:24:52 +0800

    api分组路由同步更新，保持与web分组一致

--- routers/api.go
+++ routers/api.go
@@ -7,9 +7,8 @@ import (
 	"goskeleton/app/global/variable"
 	"goskeleton/app/http/middleware/cors"
 	validatorFactory "goskeleton/app/http/validator/core/factory"
-	"io"
+	"goskeleton/app/utils/gin_release"
 	"net/http"
-	"os"
 )
 
 // 该路由主要设置门户类网站等前台路由
@@ -18,15 +17,18 @@ func InitApiRouter() *gin.Engine {
 	var router *gin.Engine
 	// 非调试模式（生产模式） 日志写到日志文件
 	if variable.ConfigYml.GetBool("AppDebug") == false {
-		//1.将日志写入日志文件
-		gin.DisableConsoleColor()
-		f, _ := os.Create(variable.BasePath + variable.ConfigYml.GetString("Logs.GinLogName"))
-		gin.DefaultWriter = io.MultiWriter(f)
-		// 2.如果是有nginx前置做代理，接口访问根本不需要gin框架记录访问日志，开启下面 2 行代码，屏蔽上面的三行代码，性能提升 5%
-		//gin.SetMode(gin.ReleaseMode)
-		//gin.DefaultWriter = ioutil.Discard
+		//1.gin自行记录接口访问日志，不需要nginx，如果开启以下3行，那么请屏蔽第 34 行代码
+		//gin.DisableConsoleColor()
+		//f, _ := os.Create(variable.BasePath + variable.ConfigYml.GetString("Logs.GinLogName"))
+		//gin.DefaultWriter = io.MultiWriter(f)
 
-		router = gin.Default()
+		//【生产模式】
+		// 根据 gin 官方的说明：[GIN-debug] [WARNING] Running in "debug" mode. Switch to "release" mode in production.
+		// 如果部署到生产环境，请使用以下模式：
+		// 1.生产模式(release) 和开发模式的变化主要是禁用 gin 记录接口访问日志，
+		// 2.go服务就必须使用nginx作为前置代理服务，这样也方便实现负载均衡
+		// 3.如果程序发生 panic 等异常使用自定义的 panic 恢复中间件拦截、记录到日志
+		router = gin_release.ReleaseRouter()
 	} else {
 		// 调试模式，开启 pprof 包，便于开发阶段分析程序性能
 		router = gin.Default()
