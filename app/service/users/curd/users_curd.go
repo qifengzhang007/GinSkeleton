f536a6da0b5c349d72731a3e7f6a210c30a1d1be
张奇峰 <1990966837@qq.com>
2022-04-08 12:23:36 +0800

    代码精简

--- app/service/users/curd/users_curd.go
+++ app/service/users/curd/users_curd.go
@@ -6,26 +6,26 @@ import (
 )
 
 func CreateUserCurdFactory() *UsersCurd {
-
-	return &UsersCurd{}
+	return &UsersCurd{model.CreateUserFactory("")}
 }
 
 type UsersCurd struct {
+	userModel *model.UsersModel
 }
 
 func (u *UsersCurd) Register(userName, pass, userIp string) bool {
 	pass = md5_encrypt.Base64Md5(pass) // 预先处理密码加密，然后存储在数据库
-	return model.CreateUserFactory("").Register(userName, pass, userIp)
+	return u.userModel.Register(userName, pass, userIp)
 }
 
 func (u *UsersCurd) Store(name string, pass string, realName string, phone string, remark string) bool {
 
 	pass = md5_encrypt.Base64Md5(pass) // 预先处理密码加密，然后存储在数据库
-	return model.CreateUserFactory("").Store(name, pass, realName, phone, remark)
+	return u.userModel.Store(name, pass, realName, phone, remark)
 }
 
 func (u *UsersCurd) Update(id int, name string, pass string, realName string, phone string, remark string, clientIp string) bool {
 	//预先处理密码加密等操作，然后进行更新
 	pass = md5_encrypt.Base64Md5(pass) // 预先处理密码加密，然后存储在数据库
-	return model.CreateUserFactory("").Update(id, name, pass, realName, phone, remark, clientIp)
+	return u.userModel.Update(id, name, pass, realName, phone, remark, clientIp)
 }
