ea051699cc25147d49cf04fc44d2cbe46d13703a
张奇峰 <1990966837@qq.com>
2022-04-14 15:23:25 +0800

    增加关于软删除的注释

--- app/model/base_model.go
+++ app/model/base_model.go
@@ -13,6 +13,7 @@ type BaseModel struct {
 	Id        int64  `gorm:"primaryKey" json:"id"`
 	CreatedAt string `json:"created_at"` //日期时间字段统一设置为字符串即可
 	UpdatedAt string `json:"updated_at"`
+	//DeletedAt gorm.DeletedAt `json:"deleted_at"`   // 如果开发者需要使用软删除功能，打开本行注释掉的代码即可，同时需要在数据库的所有表增加字段deleted_at 类型为 datetime
 }
 
 func UseDbConn(sqlType string) *gorm.DB {
