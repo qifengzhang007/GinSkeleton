f2960865c062e932bd891b0e4641e2ac7ae60b8d
张奇峰 <1990966837@qq.com>
2022-08-19 22:51:58 +0800

    websocket 建立成功之前，将错误只输出到服务器

--- app/http/validator/common/websocket/connect.go
+++ app/http/validator/common/websocket/connect.go
@@ -2,11 +2,11 @@ package websocket
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"goskeleton/app/global/consts"
 	"goskeleton/app/global/variable"
 	controllerWs "goskeleton/app/http/controller/websocket"
 	"goskeleton/app/http/validator/core/data_transfer"
-	"goskeleton/app/utils/response"
 )
 
 type Connect struct {
@@ -19,26 +19,22 @@ func (c Connect) CheckParams(context *gin.Context) {
 
 	// 1. 首先检查是否开启websocket服务配置（在配置项中开启）
 	if variable.ConfigYml.GetInt("Websocket.Start") != 1 {
-		response.Fail(context, consts.WsServerNotStartCode, consts.WsServerNotStartMsg, "")
+		variable.ZapLog.Error(consts.WsServerNotStartMsg)
 		return
 	}
 	//2.基本的验证规则没有通过
 	if err := context.ShouldBind(&c); err != nil {
-		errs := gin.H{
-			"tips": "请在get参数中提交token信息,demo格式：ws://127.0.0.1:20201?token=this is a series token...",
-			"err":  err.Error(),
-		}
-		response.ErrorParam(context, errs)
+		variable.ZapLog.Error("客户端上线参数不合格", zap.Error(err))
 		return
 	}
 	extraAddBindDataContext := data_transfer.DataAddContext(c, consts.ValidatorPrefix, context)
 	if extraAddBindDataContext == nil {
-		response.ErrorSystem(context, "websocket-Connect 表单验证器json化失败", "")
+		variable.ZapLog.Error("websocket-Connect 表单验证器json化失败")
 		context.Abort()
 		return
 	} else {
 		if serviceWs, ok := (&controllerWs.Ws{}).OnOpen(extraAddBindDataContext); ok == false {
-			response.Fail(context, consts.WsOpenFailCode, consts.WsOpenFailMsg, "")
+			variable.ZapLog.Error(consts.WsOpenFailMsg)
 		} else {
 			(&controllerWs.Ws{}).OnMessage(serviceWs, extraAddBindDataContext) // 注意这里传递的service_ws必须是调用open返回的，必须保证的ws对象的一致性
 		}
