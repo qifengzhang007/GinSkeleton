a0c2d094657e3e28d2a6b76f14a25f7e2d79e985
张奇峰 <1990966837@qq.com>
2026-04-26 18:57:30 +0800

    update: 更新注释

--- routers/grpc.go
+++ routers/grpc.go
@@ -12,7 +12,7 @@ func InitGrpcService(grpcServ *grpc.Server) {
 	// PB 文件调用注册函数，将grpc与业务service进行绑定、注册
 	// 1.注册 StudentService示例服务 - 最常见的服务定义与响应示例
 	stu_demo_pb.RegisterStudentServiceServer(grpcServ, &service_implement.StuService{})
-	// 2.注册 UserService示例服务  -  这是一种比较复杂的底层消息处理方式，一般用于开发底层库
+	// 2.注册 UserService示例服务  -  这是一种比较复杂的底层消息处理方式，一般用于开基础功能
 	user_demo_pb.RegisterUserServiceServer(grpcServ, &service_implement.UserService{})
 
 }
